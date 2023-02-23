f899e5db05f7e96f1a2ba2a8759a23d9503666af
jeff <jeff@atomrigs.io>
2023-02-23 19:54:11 +0900

    바꿔

--- src/components/slider/SliderTwo.jsx
+++ src/components/slider/SliderTwo.jsx
@@ -21,7 +21,7 @@ const SliderTwo = () => {
                         <div className="block-text pt-24 center">
                             {/*<h6 className="sub-title mb-6" data-aos="fade-up">{datatext.subtitle}</h6>*/}
                             {/*<h2 className="title mb-26" data-aos="fade-up">Most Popular NFT <br /> Collections</h2>*/}
-                            <h2 className="title mb-26" data-aos="fade-up"><img src={item.logo} alt="Monteno" /></h2>   {/* item.js 에 임의로 로고 위치지정  */}
+                            <div className="title mb-26" data-aos="fade-up"><img src={item.logo} alt="Monteno" /></div>   {/* item.js 에 임의로 로고 위치지정  */}
                             <p className="desc mb-53" data-aos="fade-up">{datatext.desc}</p>
                             {/*<Link to="/about" className="btn-action style-2" data-aos="fade-up" data-aos-duration="1200" >Get Connected</Link>*/}
                             
@@ -30,9 +30,9 @@ const SliderTwo = () => {
                                 <div className="item2 bg-2"><img src={item.item12} alt="Monteno" /></div>
                                 <div className="item3 bg-1"><img src={item.item13} alt="Monteno" /></div>
                                 <div className="item bg-1"><img src={item.item14} alt="Monteno" /></div>
-                                <div className="item2 bg-2"><img src={item.item6} alt="Monteno" /></div>
-                                <div className="item3 bg-2"><img src={item.item15} alt="Monteno" /></div>
-                                <div className="item bg-1"><img src={item.item16} alt="Monteno" /></div>
+                                {/*<div className="item bg-2"><img src={item.item6} alt="Monteno" /></div>*/}
+                                <div className="item2 bg-2"><img src={item.item15} alt="Monteno" /></div>
+                                <div className="item3 bg-1"><img src={item.item16} alt="Monteno" /></div>
                             </div>
                         </div>
                     </div>
